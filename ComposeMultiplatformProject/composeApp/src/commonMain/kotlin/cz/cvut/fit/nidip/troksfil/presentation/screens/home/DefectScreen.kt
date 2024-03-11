0e9526555d29129df6630816024a6cbe289b6d70
Filip Trokšiar <filip.troksiar@gmail.com>
2024-03-11 14:59:09 +0100

    fix: arrowBack icon fixed

--- ComposeMultiplatformProject/composeApp/src/commonMain/kotlin/cz/cvut/fit/nidip/troksfil/presentation/screens/home/DefectScreen.kt
+++ ComposeMultiplatformProject/composeApp/src/commonMain/kotlin/cz/cvut/fit/nidip/troksfil/presentation/screens/home/DefectScreen.kt
@@ -11,7 +11,7 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.BottomNavigation
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.outlined.PhotoCamera
 import androidx.compose.material3.Button
 import androidx.compose.material3.Divider
