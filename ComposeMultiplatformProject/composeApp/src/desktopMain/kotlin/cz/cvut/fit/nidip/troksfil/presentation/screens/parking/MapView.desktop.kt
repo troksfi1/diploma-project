191366edf1a79e3ac12e7944589c3b17dbcaf3a4
Filip Trokšiar <filip.troksiar@gmail.com>
2024-05-03 16:02:35 +0200

    feature desktop target MapView added

---
+++ ComposeMultiplatformProject/composeApp/src/desktopMain/kotlin/cz/cvut/fit/nidip/troksfil/presentation/screens/parking/MapView.desktop.kt
@@ -0,0 +1,10 @@
+package cz.cvut.fit.nidip.troksfil.presentation.screens.parking
+
+import androidx.compose.runtime.Composable
+
+@Composable
+actual fun MapView() {
+}
+
+actual fun moveMap(zoneName: String) {
+}
